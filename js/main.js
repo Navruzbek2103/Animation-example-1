1c8411e65b901c3b35f529907fd798f2cd982826
Navruzbek2103 <navrozbek2306@gmail.com>
2022-11-25 05:06:10 +0500

    Added animation example app

---
+++ js/main.js
@@ -0,0 +1,54 @@
+let violetCircle = 10;
+let yellowCircle = 3;
+let stringNum = String(violetCircle + yellowCircle);
+function viewCircle(){
+  setTimeout(() => {
+    $(".wrapper-title").style.transform = 'translateX(0)'
+    $(".wrapper-text").style.transform = 'translateX(0)'
+  }, 300);
+
+  for(let i = 0; i < violetCircle; i++){
+    let circle = createElement("li", "inner-left-item");
+    $(".inner-left-list").appendChild(circle)
+  }
+  for(let i = 0; i < yellowCircle; i++){
+    let circle = createElement("li", "inner-right-item");
+    $(".inner-right-list").appendChild(circle)
+  }
+}
+viewCircle()
+let audio = new Audio("./../audio/finish.ogg")
+function calculateExample(){
+  let enterNum = 0;
+  $(".btn-group").addEventListener("click", (e)=>{
+    let counter = stringNum[enterNum];
+    switch(enterNum){
+      case 0: $(".firstValue").value = e.target.textContent; break;
+      case 1: $(".secondValue").value = e.target.textContent; break
+    }
+
+    if(e.target.textContent === counter){
+      $(".inner-left-list").classList.remove("showCircleError")
+      $(".inner-right-list").classList.remove("showCircleError")
+      $(".input-group").style.border = "8px solid #CFCECE"
+
+      enterNum++; 
+    }
+    else{
+      if(enterNum == 0) $(".inner-left-list").classList.add("showCircleError")
+      if(enterNum == 1) $(".inner-right-list").classList.add("showCircleError")
+      $(".input-group").style.border = "8px solid #ED696A"
+    }
+    if(enterNum == 2){
+      $(".wrapper").style.transform = "translateX(-200%)";
+      $(".wrapper").style.transition = "transform .7s ease-in";
+      audio.play();
+      $(".next-page").style.transform = "translateX(0)";
+    }
+  })
+  $(".reload-img").addEventListener("click", () => {
+    window.location.reload()
+  })
+}
+
+calculateExample()
---
+++ js/plugin.js
@@ -0,0 +1,19 @@
+let $ = function (selector){
+  return document.querySelector(selector)
+}
+
+let $$ = function (selector){
+  return document.querySelectorAll(selector)
+}
+
+let createElement = function(tagName, className, content){
+  const newElement = document.createElement(tagName);
+  if(className){
+    newElement.setAttribute("class", className)
+  }
+  if(content){
+    newElement.innerHTML = content
+  }
+
+  return newElement
+}
